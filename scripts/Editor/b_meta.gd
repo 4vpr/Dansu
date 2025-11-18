d9c0c1bc69372c2193f5bcf84ea269f8a481a7d8
h4yase <108639830+4vpr@users.noreply.github.com>
2025-11-18 13:04:49 +0900

    Delete scripts/Editor directory

--- scripts/Editor/b_meta.gd
+++
@@ -1,7 +0,0 @@
-extends Button
-func _ready() -> void:
-	pressed.connect(_on_button_pressed)
-	pass
-func _on_button_pressed():
-	$"../Song Setup".visible = true
-	pass
--- scripts/Editor/button.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-func _ready() -> void:
-	connect("pressed", _on_pressed)
-func _on_pressed():
-	if CM.sc:
-		if Game.scene == Game.Scene.Play:
-			get_tree().change_scene_to_file("res://scene/play_scene.tscn")
-		else:
-			get_tree().change_scene_to_file("res://scene/map_editor.tscn")
